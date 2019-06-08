d96ec1e634d489d3390ae4f9e294c93a017933d8
Sergey <12kaptan12@gmail.com>
2019-06-08 18:01:49 +0300

    add MVC

---
+++ GitHubJob(MVC)/View Controller/GitHubDetailsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubDetailsViewController.swift
+//  GitHubJob(MVC)
+//
+//  Created by Sergey Koriukin on 08/06/2019.
+//  Copyright © 2019 Sergey Koriukin. All rights reserved.
+//
+
+import UIKit
+
+class GitHubDetailsViewController: UIViewController {
+
+    var item: Item!
+   
+    @IBOutlet weak var companyDetailLogo: UIImageView!
+    
+}
